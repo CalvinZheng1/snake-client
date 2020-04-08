858013b086403618f43323757b3288a36754ac1e
Calvin Zheng <calvinzheng08@hotmail.com>
2020-04-08 20:04:54 +0000

    commit

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '50.64.116.162',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled');
-  });
-  return conn;
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
-
